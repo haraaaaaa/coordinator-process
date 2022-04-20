66cf0b75c30e629aceea94fdca9449a997b78db3
konsent <konsent2@gmail.com>
2022-04-20 22:52:52 +0900

    Setting communication between API and Process Server

--- getWeatherData.py
+++ getWeatherData.py
@@ -1,13 +1,16 @@
 import asyncio
 import websockets
 
+async def connect():
+    async with websockets.connect("ws://localhost:9998/websocket") as websocket:
+        await websocket.send("GET");
+        print("GET Request Completed");
+        JSON = await websocket.recv();
+        print(JSON)
 
-async def accept(websocket, path):
-    data = await websocket.recv();
-    print(data);
+# 비동기로 서버에 접속한다.
+loop = asyncio.get_event_loop()
+loop.run_until_complete(connect())
+loop.close()
 
-start_server = websockets.serve(accept, "localhost", 9998);
-
-asyncio.get_event_loop().run_until_complete(start_server);
-asyncio.get_event_loop().run_forever();
 
